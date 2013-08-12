e1638763506183531295f208e721988d5032cdb0
smgi <your.email@example.com>
2013-08-12 14:11:16 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '9944882352913c45caad9a62b11d51fff4a21ad7428cf2eb4f5d63e722efd64939037de7090b2b2221b44eb1b050c171099831c0edc3829c75568d6a712d94cf'
